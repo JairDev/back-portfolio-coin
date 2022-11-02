6d96ef4584b582ace176ea76bb8486ba962dec25
jairdev <devalfredomoscoso@gmail.com>
2022-11-02 13:27:18 -0400

    Build

--- dist/db/dbConnect.js
+++ dist/db/dbConnect.js
@@ -12,8 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = require("dotenv");
+const mongoose_1 = __importDefault(require("mongoose"));
 (0, dotenv_1.config)();
 const DB_URL = process.env.DB_URL;
 function dbConnect() {
